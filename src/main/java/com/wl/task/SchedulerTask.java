061f275dce7a6b5d37ef3b602d4fbae0d6d84073
WeiLin <weilint@163.com>
2019-05-22 11:09:46 +0800

    定时任务

---
+++ src/main/java/com/wl/task/SchedulerTask.java
@@ -0,0 +1,33 @@
+package com.wl.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Task - 定时任务
+ *
+ * @author WeiLin
+ * @version 1.0
+ */
+@Component
+public class SchedulerTask {
+
+    private int count = 0;
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+
+    @Scheduled(cron = "*/6 * * * * ?")
+    private void process() {
+        System.out.println("this is scheduler task running " + (count++));
+    }
+
+
+    @Scheduled(fixedRate = 6000)
+    public void reportCurrentTime() {
+        System.out.println("现在时间：" + dateFormat.format(new Date()));
+    }
+}
